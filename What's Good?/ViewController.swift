74f8e2f44d9bce3e6a73c425680212aa4ae865f7
Raghav <raghav.kedia@duke.edu>
2016-03-16 14:27:23 -0400

    Initial Commit

---
+++ What's Good?/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  What's Good?
+//
+//  Created by Raghav Kedia on 3/16/16.
+//  Copyright © 2016 Raghav Kedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
